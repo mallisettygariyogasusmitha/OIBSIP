9049882670d5d3d8ef048b516d5f57d986b57f2a
mallisettygariyogasusmitha <164845085+mallisettygariyogasusmitha@users.noreply.github.com>
2024-04-17 22:12:00 +0530

    Add files via upload

---
+++ BMIcalculator.py
@@ -0,0 +1,21 @@
+height=float(input("Enter the height(cm):"))
+weight=float(input("Enter the Weight(kg):"))
+h=height/100
+bmi=weight/(h**2)
+if(bmi<=16):
+        print(bmi,"\nSevere Thinness")
+elif(16<bmi<=17):
+        print(bmi,"\nMild Thinness")
+elif(17<bmi<=18.5):
+        print(bmi,"\nModerate Thinness")
+elif(18.5<bmi<=25):
+        print(bmi,"\nNormal")
+elif(25<bmi<=30):
+        print(bmi,"\nOverWeight")
+elif(30<bmi<=35):
+        print(bmi,"\nObese Class I")
+elif(35<bmi<=40):
+        print(bmi,"\nObese Class II")
+elif(bmi>40):
+        print(bmi,"\nObese Class III")
+    
